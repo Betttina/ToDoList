8996550f5337497d03b6296fd2854680962d6983
Betttina <bettina.toth@gritacademy.se>
2024-08-05 21:14:19 +0200

    first commit
    set up backend:
    - install packages: mongoose, cors, express.
    - database connection
    - data model schema for ToDo
    - define routes (api-endpoints) for CRUD-operations (+ handle success and error)

---
+++ server/routes/todos.js
@@ -0,0 +1,57 @@
+const router = require('express').Router();
+let Todo = require('../models/todo.model');
+
+// get all posts
+router.route('/').get((req, res) => {
+    Todo.find()
+        .then(todos => res.json(todos))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// create a new post
+router.route('/add').post((req, res) => {
+    const title = req.body.title;
+    const description = req.body.description;
+    const completed = req.body.completed;
+
+    const newTodo = new Todo({
+        title,
+        description,
+        completed,
+    });
+
+    newTodo.save()
+        .then(() => res.json('To-Do added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// get a specific post
+router.route('/:id').get((req, res) => {
+    Todo.findById(req.params.id)
+        .then(todo => res.json(todo))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// delete a spec post
+router.route('/:id').delete((req, res) => {
+    Todo.findByIdAndDelete(req.params.id)
+        .then(() => res.json('To-Do deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// update spec post
+router.route('/update/:id').post((req, res) => {
+    Todo.findById(req.params.id)
+        .then(todo => {
+            todo.title = req.body.title;
+            todo.description = req.body.description;
+            todo.completed = req.body.completed;
+
+            todo.save()
+                .then(() => res.json('To-Do updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// connect to MongoDB
+const uri = 'mongodb://localhost:27017/todolist';
+mongoose.connect(uri);
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully');
+});
+
+
+// import todos
+const todoRouter = require('./routes/todos');
+app.use('/todos', todoRouter);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`To-do-backend app listening on port ${port}`)
+})
